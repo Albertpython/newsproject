729f6282074de7a5b05e206f78d45a8d8f98d1ec
Albertpython <80157870+Albertpython@users.noreply.github.com>
2021-05-29 11:29:56 +0400

    "news"

--- mynews/polls/urls.py
+++ mynews/polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('category/<slug:slug>', Categories),
-    path('', NewsListView.as_view(), name='title'),
-    path('new/<slug:slug>', NewsDetailView.as_view(), name='title'),
-    path('new/<slug:slug>', CategoryDetail.as_view(), name='title')
+    path('', NewsListView.as_view(), name='home'),
+    path('new/<slug:slug>', NewsDetailView.as_view(), name='new'),
+    path('test/<slug:slug>', CategoryDetail.as_view(), name='title')
+    # path('r/register/$', RegisterFormView.as_view()),
+    # path('r/login/$', LoginFormView.as_view()),
+    # path('r/logout/$', LogiutFormView.as_view()),
 ]
--- mynews/polls/views.py
+++ mynews/polls/views.py
@@ -2,33 +2,33 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Category, New
 
-class Categories:
-    """ Get Categories """
 
-    def get_categories(self):
-        return Category.objets.all()
 
-class NewsListView(Categories, ListView):
+class NewsListView(ListView):
     """ News """
     model = New
     queryset = New.objects.filter(status='published').order_by('-date')
     template_name = 'news_list.html'
     paginate_by = 10
 
-class NewsDetailView(Categories, DetailView):
-    """ News new page """
-
-    model = New
-    template_name = 'news_detail.html'
-    context_object_name = 'New'
-
     def get_context_data(self, **kwargs):
-        context = super(NewsDetailView, self).get_context_data(**kwargs)
-        context['additional'] = New.objects.filter(status='published').order_by('-date')
+        context = super(NewsListView, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
         return context
 
 
-class CategoryDetail(Categories, ListView):
+class NewsDetailView(ListView):
+    """ News new page """
+
+    model = Category
+    template_name = 'news_detail.html'
+    def get_queryset(self, **kwargs):
+        queryset = New.objects.filter(category__slug=self.kwargs['slug'], status='published').order_by('-date')
+        return queryset
+
+
+
+class CategoryDetail( ListView):
     """ News by category """
 
     template_name = 'category_detail.html'
